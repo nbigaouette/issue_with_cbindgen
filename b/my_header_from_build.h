a7ae5a25941a33ea245d1a0475df32addfbce48c
Nicolas Bigaouette <nbigaouette@gmail.com>
2024-03-07 16:42:09 -0500

    Add generated headers

---
+++ b/my_header_from_build.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/* Generated with cbindgen:0.26.0 */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t free_standing_function(const TypeA *grand_parent);
---
+++ b/my_header_from_cli.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* Generated with cbindgen:0.26.0 */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct TypeA {
+  uint32_t aa;
+  uint32_t ab;
+} TypeA;
+
+uint32_t free_standing_function(const struct TypeA *grand_parent);
